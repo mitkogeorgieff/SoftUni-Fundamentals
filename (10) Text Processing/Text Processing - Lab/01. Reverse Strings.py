f2f0e3c4f1850237bafeb7a26d3dfb75443792cb
Dimitar Georgiev <mitkogeorgieff@gmail.com>
2023-03-14 19:07:22 +0200

    Lecture - Text Processing

---
+++ (10) Text Processing/Text Processing - Lab/01. Reverse Strings.py
@@ -0,0 +1,9 @@
+def reverse_string(string: str):
+    reversed_string = string[::-1]
+    return f'{string} = {reversed_string}'
+
+
+string = input()
+while string != "end":
+    print(reverse_string(string))
+    string = input()
